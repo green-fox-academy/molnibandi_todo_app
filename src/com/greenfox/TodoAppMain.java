140926b92cf128d525d18e6068768a028e83bfe9
molnibandi <62934438+molnibandi@users.noreply.github.com>
2020-12-11 15:23:42 +0100

    Main was finished

--- src/com/greenfox/TodoAppMain.java
+++ src/com/greenfox/TodoAppMain.java
@@ -8,34 +8,24 @@ import java.util.List;
 public class TodoAppMain {
 
     public static void main(String[] args) {
-        if (args.length > 0) {
-            for (String s : args) {
-                switch (s) {
-                    case "-a":
-                        add();
-                        break;
-                    case "-l":
-                        list();
-                        break;
-                    case "-r":
-                        remove();
-                        break;
-                    case "-c":
-                        complete();
-                        break;
-                    default:
-                        printMenu();
-                }
-            }
-        } else {
-            printMenu();
 
+        ArgumentHandler handler = new ArgumentHandler(args);
+
+        if (handler.noArgument()) {
+            printMenu();
+        } else if (!handler.validArg() || !handler.validLength()) {
+            System.out.println("Unsupported argument");
+            printMenu();
+        } else {
+            TaskHandler myTodoList = new TaskHandler(args);
+            myTodoList.handleList();
         }
     }
 
+
     private static void printMenu() {
         try {
-            Path filePath = Paths.get("C:\\Users\\molna\\Desktop\\greenfox\\molnibandi_todo_app\\out\\production\\molnibandi_todo_app\\todoMenu.txt");
+            Path filePath = Paths.get("todoMenu.txt");
             List<String> lines = Files.readAllLines(filePath);
             for (String line : lines) {
                 System.out.println(line);
@@ -44,21 +34,5 @@ public class TodoAppMain {
         } catch (Exception e) {
             System.out.println("Not able to read the file!");
         }
-    }
-
-    public static void add() {
-        System.out.println("Add a task");
-    }
-
-    public static void list() {
-        System.out.println("List tasks");
-    }
-
-    public static void remove() {
-        System.out.println("Remove a task");
-    }
-
-    public static void complete() {
-        System.out.println("Complete a task");
     }
 }
